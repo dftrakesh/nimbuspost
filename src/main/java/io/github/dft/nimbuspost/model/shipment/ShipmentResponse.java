f72c614493dd2bc7f1f017776698d945f61638fa
Shivangee.Godhani <shivangee.godhani@dakshyafreondtech.com>
2023-09-15 18:54:57 +0530

    change version Add message field

--- src/main/java/io/github/dft/nimbuspost/model/shipment/ShipmentResponse.java
+++ src/main/java/io/github/dft/nimbuspost/model/shipment/ShipmentResponse.java
@@ -1,11 +1,14 @@
 package io.github.dft.nimbuspost.model.shipment;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
 @lombok.Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class ShipmentResponse {
     private Boolean status;
     private Data data;
+    private String message;
 }
