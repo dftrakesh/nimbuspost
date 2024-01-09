70d575c7438c2e911b59e8c57c4dff88b4c6d8bd
Shivangee.Godhani <shivangee.godhani@dakshyafreondtech.com>
2024-01-09 11:12:15 +0530

    GODAM-158 Add CancelShipmentRequest and CancelShipmentResponse. Add cancelShipment method.

--- src/main/java/io/github/dft/nimbuspost/model/shipment/CancelShipmentResponse.java
+++ src/main/java/io/github/dft/nimbuspost/model/shipment/CancelShipmentResponse.java
@@ -1,12 +1,6 @@
 package io.github.dft.nimbuspost.model.shipment;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
-
 @lombok.Data
-@JsonIgnoreProperties(ignoreUnknown = true)
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class CancelShipmentResponse {
     private Boolean status;
     private String message;
