41a6277fe623f03f3926d9a67ed131358a3f5247
Shivangee.Godhani <shivangee.godhani@dakshyafreondtech.com>
2024-01-09 11:10:24 +0530

    GODAM-158 Add CancelShipmentRequest and CancelShipmentResponse. Add cancelShipment method.

--- src/main/java/io/github/dft/nimbuspost/NimbusShipmentAPI.java
+++ src/main/java/io/github/dft/nimbuspost/NimbusShipmentAPI.java
@@ -1,15 +1,20 @@
 package io.github.dft.nimbuspost;
 
 import io.github.dft.nimbuspost.auth.AuthCredentials;
+import io.github.dft.nimbuspost.model.shipment.CancelShipmentRequest;
+import io.github.dft.nimbuspost.model.shipment.CancelShipmentResponse;
 import io.github.dft.nimbuspost.model.shipment.ShipmentRequest;
 import io.github.dft.nimbuspost.model.shipment.ShipmentResponse;
+
 import java.net.URI;
 import java.net.http.HttpRequest;
+
 import static io.github.dft.nimbuspost.constantcodes.ConstantCode.API_BASE_URL;
 
 public class NimbusShipmentAPI extends NimbusSDK {
 
     private final String SHIPMENT_ENDPOINT = "/shipments";
+    private final String CANCEL_ENDPOINT = "/cancel";
 
     public NimbusShipmentAPI(AuthCredentials authCredentials) {
         super(authCredentials);
@@ -20,5 +25,11 @@ public class NimbusShipmentAPI extends NimbusSDK {
         HttpRequest request = post(baseUrl, shipmentRequest);
 
         return getRequestWrapped(request, ShipmentResponse.class);
+    }
+
+    public CancelShipmentResponse cancelShipment(CancelShipmentRequest cancelShipmentRequest) {
+        URI baseUrl = baseUrl(API_BASE_URL, SHIPMENT_ENDPOINT + CANCEL_ENDPOINT);
+        HttpRequest request = post(baseUrl, cancelShipmentRequest);
+        return getRequestWrapped(request, CancelShipmentResponse.class);
     }
 }
---
+++ src/main/java/io/github/dft/nimbuspost/model/shipment/CancelShipmentResponse.java
@@ -0,0 +1,13 @@
+package io.github.dft.nimbuspost.model.shipment;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@lombok.Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class CancelShipmentResponse {
+    private Boolean status;
+    private String message;
+}
